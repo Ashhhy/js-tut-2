ab786cd31d7b813c936cc7d4873423572c23bb03
Ashhh <tanishqmeshram99@gmail.com>
2025-06-23 22:24:19 +0530

    conversion types - L1

---
+++ tut1.js
@@ -0,0 +1,31 @@
+let score = NaN;
+console.log(typeof(score));
+
+let inumber = Number(score);
+console.log(typeof(value));
+
+//score = 10 => 10
+//score = "10" => NaN
+//score = "sgsg"= > NaN
+//score = undefined => NaN
+//score = null => 0
+//score = true => 1
+//score = NaN 
+
+//convert boolean
+let isyes= true;
+let isno = Boolean(isyes);
+console.log(isno);
+
+//1 => true; 0 => false
+// "" => false
+// "Something" => true
+
+//convert string
+let str = 33;
+let strnum = String(str);
+console.log(strnum);
+console.log(typeof(strnum));
+
+
+
