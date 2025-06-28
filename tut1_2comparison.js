e1dfe1a4450938098d60fba51966103be92d6c05
Ashhh <tanishqmeshram99@gmail.com>
2025-06-28 18:14:21 +0530

    Adding comparison notes

---
+++ tut1_2comparison.js
@@ -0,0 +1,21 @@
+
+
+
+
+console.log(null > 0); //false
+console.log(null == 0);//false
+console.log(null >= 0);//true
+
+//comparision operator converts null to 0 whereas equality operator does not
+
+//------------------------------------------------------------------------------------------
+
+//strict check(===) does not convert types(it checks vlaue and also the data types)
+console.log("2" == 2); //true
+console.log("2" === 2); //false
+
+
+
+
+
+
---
+++ tut1_3datatypes.js
@@ -0,0 +1,12 @@
+
+/* 
+
+Twotypes of data types based on storung and accessing data - 
+--Primitive
+--Non-Primitive(Reference) data types 
+
+*/
+
+//Primitive data types
+
+
